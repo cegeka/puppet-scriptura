928746b74a7fd6449d513ff66f794e44c512c2bc
Ausloos Kim <kim.ausloos@cegeka.be>
2018-08-20 12:44:57 +0200

    [scriptura] Upgrade scriptura to 18.04, add cassandra support

---
+++ manifests/iac/cassandra.pp
@@ -0,0 +1,36 @@
+define scriptura::iac::cassandra(
+  $version=undef,
+  $versionlock=false
+) {
+  if ! $version {
+    fail('Class[Scriptura::Iac::Cassandra]: parameter version must be provided')
+  }
+
+  package { 'scriptura-cassandra':
+    ensure => $version
+  }
+
+  case $versionlock {
+    true: {
+      packagelock { 'scriptura-cassandra':
+        require => Package['scriptura-cassandra']
+      }
+    }
+    false: {
+      packagelock { 'scriptura-cassandra':
+        ensure  => absent,
+        require => Package['scriptura-cassandra']
+      }
+    }
+    default: { fail('Class[Scriptura::Iac::Cassandra::Package]: parameter versionlock must be true or false')}
+  }
+
+  service { 'scriptura-cassandra':
+    ensure     => running,
+    enable     => true,
+    hasstatus  => true,
+    hasrestart => true,
+    require    => Package['scriptura-cassandra']
+  }
+
+}
